b98663cca4a625fc180a293f143cca112b9f5e5b
Joshua Morton <joshanator1995@gmail.com>
2015-09-03 16:02:53 +1000

    made facebook module extract time of posts

--- site/test.py
+++ site/test.py
@@ -1,3 +1,17 @@
 from facebook import Facebook
+import json
+import time
 
-print(Facebook.getFeed())
+feed = Facebook.getFeed()
+
+#t = parseTime(feed[0]['updated_time'])
+#print(time.strftime('%a, %d %b %Y %H:%M:%S +0000', t))
+
+def formatTime(t):
+    return time.strftime('%a, %d %b %Y %H:%M:%S +0000', t)
+
+for post in feed:
+    print(formatTime(post['time']))
+    print(post['post_id'])
+    print(post['message'])
+
