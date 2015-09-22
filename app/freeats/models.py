2dea000fa665e41c7f2ab50773414986d8533f03
danielbu329 <daniel.bu329@gmail.com>
2015-09-22 23:56:04 +1000

    added food model

--- app/freeats/models.py
+++ app/freeats/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Food(models.Model):
+    title = models.CharField(max_length=40)
+    location = models.CharField(max_length=40)
+    description = models.CharField("Description of Food", max_length=200)
+    date = models.DateField()
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+    author = models.CharField(max_length=100)
+    imgurl = models.CharField(max_length=100)
