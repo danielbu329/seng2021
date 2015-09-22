b17e7765e335e9eba4d61daabcd1dc5b9b6f3c30
Joshua Morton <joshanator1995@gmail.com>
2015-09-22 15:19:03 +1000

    moved create post into templates folder

--- app/freeats/views.py
+++ app/freeats/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'freeats/index.html', {})
+
+def createpost(request):
+	return render(request, 'freeats/createpost.html', {})
