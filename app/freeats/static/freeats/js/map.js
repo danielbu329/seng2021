6d5d882e7cc272f9a13f27df14be0424e8921064
Joshua Morton <joshanator1995@gmail.com>
2015-09-24 09:50:16 +1000

    made map fade in only after it's fully loaded

--- app/freeats/static/freeats/js/map.js
+++ app/freeats/static/freeats/js/map.js
@@ -8,6 +8,7 @@ function ($, google) {
   var globeLawn = { lat: -33.917970, lng: 151.231202};
   var unsw = {lat: -33.9178745, lng: 151.2306935};
 
+  $($('.map')[0]).css({ opacity: 0 });
   var map = new google.maps.Map($('.map')[0], {
     center: unsw,
     zoom: 17,
@@ -24,6 +25,11 @@ function ($, google) {
 
   google.maps.event.addListener(map, 'click', function(event) {
     addMarker(event.latLng, map);
+  });
+
+  google.maps.event.addListener(map, 'tilesloaded', function () {
+    console.log('map loaded');
+    $($('.map')[0]).fadeTo(200, 1);
   });
 
   addMarker(globeLawn, map);
