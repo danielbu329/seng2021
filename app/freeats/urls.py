86c0f7f777860352ceae753a3f1e1a6350e9d2d8
danielbu329 <daniel.bu329@gmail.com>
2015-09-24 15:43:57 +1000

    url food

--- app/freeats/urls.py
+++ app/freeats/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^createpost$', views.createpost, name='createpost'),
-    url(r'^test$', views.test, name='test')
+    url(r'^test$', views.test, name='test'),
+    url(r'^food$', views.food, name='food')
 ]
