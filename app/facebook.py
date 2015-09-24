9fc19d34b52bfdbc77a87e8e2076f32e3646803b
Christopher Chaaya <z5016114@student.unsw.edu.au>
2015-09-24 22:17:34 +1000

    integrated locations and filtering -- mostly done

--- app/facebook.py
+++ app/facebook.py
@@ -27,27 +27,68 @@ class Facebook:
                 continue
             feed.append({
                 'post_id': i['id'],
-                'time': parseTime(i['updated_time']),
+                'time': (parseTime(i['updated_time'])),
                 'message': i['message']
             })
 
         return feed
 
-f = Facebook()
-messages = f.getFeed()
-filtered = []
+def filterPosts():
+    f = Facebook()
+    messages = f.getFeed()
+    filtered = []
 
-foods = [line.rstrip('\n') for line in open('foods.txt')]
+    foods = [line.rstrip('\n') for line in open('foods.txt')]
+    locations = [line.rstrip('\n') for line in open('locations.txt')]
 
-for m in messages:
-    for f in foods:
-        if f in m['message']:
-            filtered.append({
-                'post_id': m['post_id'],
-                'time': m['time'],
-                'message': m['message']
-            })
+    for m in messages:
+        new = ({
+            'done': 0,
+            'otherfoods': '',
+            'post_id': '',
+            'mainfoodtype': '',
+            'time': '',
+            'location': ''
+        });
+        found = 0
+        for f in foods:
+            if f.lower() in m['message'].lower():
+                found = 1
+                new['post_id'] = m['post_id']
+                new['time'] = m['time']
 
-for i in filtered:
-    print (i, "\n")
+                if (new['done'] == 0):
+                    new['mainfoodtype'] = f   
+                    new['done'] = 1               
+                else:
+                    new['otherfoods'] += " " + f
+
+                for l in locations:
+                    if l.lower() in m['message'].lower():
+                        new['location'] = l
+                
+        if found == 1:
+            filtered.append(new)
+
+
+    for i in filtered:
+        print ("----------------")
+        print ("Food Type: ", i['mainfoodtype'])
+        print ("Other Food Types: ", i['otherfoods'])
+        print ("Time Posted:", i['time'])
+        print ("Post ID:", i['post_id'])
+        print ("Location:", i['location'])
+
+    return filtered
+
+
+def printAll():
+    f = Facebook()
+    messages = f.getFeed()
+    for m in messages:
+        print (m['message'], "\n")
+
+filterPosts()
+print ("--------------------------------------\n")
+#printAll()
 
