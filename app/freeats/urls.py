9b8623859a4ab68d44dabb7150cdbd92968810a7
danielbu329 <daniel.bu329@gmail.com>
2015-09-24 16:33:19 +1000

    /freeats/food returns json format of data

--- app/freeats/urls.py
+++ app/freeats/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^createpost$', views.createpost, name='createpost'),
     url(r'^test$', views.test, name='test'),
-    #url(r'^food$', views.food, name='food')
+    url(r'^food$', views.food, name='food')
 ]
--- app/freeats/views.py
+++ app/freeats/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+import json
+from .models import Food
 
 def index(request):
     return render(request, 'freeats/index.html', {})
@@ -8,3 +12,9 @@ def createpost(request):
 
 def test(request):
 	return render(request, 'freeats/test.html', {})
+
+def food(request):
+    if request.method == "GET":
+        foods = Food.objects.all()
+        foodData = serializers.serialize('json', foods)
+        return HttpResponse(foodData, content_type='application/json')
