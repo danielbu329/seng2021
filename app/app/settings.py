9ff150e7f0651b4766146ed11d171813cb2b386c
Paul Jukic <paul.jukic@hotmail.com>
2015-10-15 18:26:36 +1100

    setup for heroku

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,8 +25,6 @@ SECRET_KEY = '+!p2i!=5^l*y3xq8nmyrhvbe2n)jl96=f1@gf!688+w(0+v=gl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -101,7 +99,20 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
