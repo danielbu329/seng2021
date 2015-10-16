87254b7ac8dd29780ddc3c3b991d4cbb5a8db42b
Joshua Morton <joshanator1995@gmail.com>
2015-10-16 11:21:04 +1100

    renamed site to app

--- app/freeats/models.py
+++ app/freeats/models.py
@@ -1,20 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Food(models.Model):
-    title = models.CharField(max_length=40)
-    location = models.CharField(max_length=40)
-    description = models.CharField("Description of Food", max_length=400)
-    date = models.DateField()
-    likes = models.IntegerField()
-    dislikes = models.IntegerField()
-    author = models.CharField(max_length=50)
-    imgurl = models.CharField(max_length=100)
-
-class User(models.Model):
-    #username = models.CharField(max_length=50)
-    #password = models.CharField(max_length=50)
-    #email = models.CharField(max_length=100)
-    fb_user_id = models.IntegerField()
-    admin_status = models.BooleanField(default=0);
