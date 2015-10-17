e1042a76e043d4d0f14ba42c90debb4a96c3035c
Joshua Morton <joshanator1995@gmail.com>
2015-10-17 22:10:18 +1100

    cleaned up views.py and moved some logic into utils.py

--- app/freeats/views.py
+++ app/freeats/views.py
@@ -5,7 +5,7 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Sum, Count
 import json
 from .models import Food, User, Vote
-from .facebook import Facebook
+from .utils import *
 
 def index(request):
     return render(request, 'freeats/index.html', {})
@@ -15,46 +15,31 @@ def index(request):
 # POST uses same variable names as model, gets each entry
 # then inserts it into to the database
 def food(request):
-    if request.method == "GET":
-        f = Facebook()
-        user_id = f.authorize(request.GET.get('user_id'), request.GET.get('access_token'))
-        fb_user = None
-        if User.objects.filter(fb_user_id=user_id).exists():
-            fb_user = User.objects.get(fb_user_id=user_id)
-        else:
-            fb_user = User(fb_user_id=user_id)
-            fb_user.save()
+    user_id = authenticate(request)
+    if request.method == 'GET':
         # The '-' in -creation-time makes it sort in descending order
-        foods = Food.objects \
+        foods = list(Food.objects \
             .annotate(likes=Sum('vote__like'), votes=Count('vote')) \
             .order_by('-creation_time') \
-            .values()
-        foods = list(foods)
-        if len(user_id) > 0:
+            .values())
+        if user_id:
             # User is logged in
+            fb_user = getUserOrCreate(user_id)
             for i in foods:
                 if Vote.objects.filter(food=i['id'], fb_user=fb_user).exists():
                     vote = Vote.objects.get(food=i['id'], fb_user=fb_user).like
                     i['vote'] = 'up' if vote == 1 else 'down'
         foodData = json.dumps(foods, cls=DjangoJSONEncoder)
         return HttpResponse(foodData, content_type='application/json')
-    if request.method == "POST":
-        data = json.loads(request.body.decode('utf-8'))
-        title = data['title'] if 'title' in data else ''
-        location = data['location'] if 'location' in data else ''
-        description = data['description'] if 'description' in data else ''
-        likes = 0
-        dislikes = 0
-        if 'user_id' in data and 'access_token' in data:
-            f = Facebook()
-            user_id = f.authorize(data['user_id'], data['access_token'])
-            fb_user = None
-            if User.objects.filter(fb_user_id=user_id).exists():
-                fb_user = User.objects.get(fb_user_id=user_id)
-            else:
-                fb_user = User(fb_user_id=user_id)
-                fb_user.save()
+    if request.method == 'POST':
+        if user_id:
+            # User is logged in
+            data = json.loads(request.body.decode('utf-8'))
+            title = data['title'] if 'title' in data else ''
+            location = data['location'] if 'location' in data else ''
+            description = data['description'] if 'description' in data else ''
             img_url = ''
+            fb_user = getUserOrCreate(user_id)
             new_food = Food(
                     title=title, location=location, description=description,
                     fb_user=fb_user, img_url=img_url)
@@ -64,50 +49,37 @@ def food(request):
 # freeats/vote
 # POST
 def vote(request):
+    user_id = authenticate(request)
     if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         postId = data['postId'] if 'postId' in data else None
         vote = data['vote'] if 'vote' in data else None
+        like = 0
         if vote == 'up':
-            vote = 1
-        elif vote == 'down':
-            vote = 0
-        else:
-            vote = 0
-        if 'user_id' in data and 'access_token' in data:
-            f = Facebook()
-            user_id = f.authorize(data['user_id'], data['access_token'])
-            fb_user = None
-            if User.objects.filter(fb_user_id=user_id).exists():
-                fb_user = User.objects.get(fb_user_id=user_id)
-            else:
-                fb_user = User(fb_user_id=user_id)
-                fb_user.save()
+            like = 1
+        if user_id:
+            # User is logged in
+            fb_user = getUserOrCreate(user_id)
             if postId != None and Food.objects.filter(id=postId).exists():
                 food = Food.objects.get(id=postId)
-                v = None
+                vote = None
                 if Vote.objects.filter(fb_user=fb_user, food=food).exists():
-                    v = Vote.objects.get(fb_user=fb_user, food=food)
-                    v.like = vote
+                    vote = Vote.objects.get(fb_user=fb_user, food=food)
+                    vote.like = like
                 else:
-                    v = Vote(fb_user=fb_user, food=food, like=vote)
-                v.save()
+                    vote = Vote(fb_user=fb_user, food=food, like=like)
+                vote.save()
         return HttpResponse("saved vote");
 
 # freeats/myposts
 def myposts(request):
-    f = Facebook()
-    user_id = f.authorize(request.GET.get('user_id'), request.GET.get('access_token'))
-    fb_user = None
-    if User.objects.filter(fb_user_id=user_id).exists():
-        fb_user = User.objects.get(fb_user_id=user_id)
-    else:
-        fb_user = User(fb_user_id=user_id)
-        fb_user.save()
-    # The '-' in -creation-time makes it sort in descending order
-    foods = Food.objects \
-        .filter(fb_user=fb_user.id) \
-        .order_by('-creation_time') \
-        .values()
-    foodData = json.dumps(list(foods), cls=DjangoJSONEncoder)
+    user_id = authenticate(request)
+    if user_id:
+        fb_user = getUserOrCreate(user_id)
+        # The '-' in -creation-time makes it sort in descending order
+        foods = Food.objects \
+            .filter(fb_user=fb_user.id) \
+            .order_by('-creation_time') \
+            .values()
+        foodData = json.dumps(list(foods), cls=DjangoJSONEncoder)
     return HttpResponse(foodData, content_type='application/json')
