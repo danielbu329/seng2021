0869e7e7b5403537224d8de3947f279d704bcca5
Joshua Morton <joshanator1995@gmail.com>
2015-10-21 15:18:58 +1100

    changed opacity of map background

--- app/freeats/static/freeats/js/map.js
+++ app/freeats/static/freeats/js/map.js
@@ -108,7 +108,7 @@ function ($, google, eventBus) {
       strokeOpacity: 0.8,
       strokeWeight: 2,
       fillColor: "#000000",
-      fillOpacity: 0.2
+      fillOpacity: 0.1
     });
     unswTrace.setMap(map);
   };
